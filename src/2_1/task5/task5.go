39b7b3424afe50d8706bbf02e1c109d1a4ec4b70
gaylesha <petr.vasilyev.1999@gmail.com>
2025-05-07 23:17:07 +0500

    del

---
+++ src/2_1/task5/task5.go
@@ -0,0 +1,7 @@
+package task5
+
+import "fmt"
+
+func MainTask5() {
+	fmt.Println("Hello from task 2_13!")
+}
---
+++ src/2_1/task9/task9.go
@@ -0,0 +1,10 @@
+package task9
+
+import "fmt"
+
+func MainTask9() {
+	fmt.Println("row1")
+	fmt.Println("row2")
+	fmt.Println("row3")
+	fmt.Println("row4")
+}
