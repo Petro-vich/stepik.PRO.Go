d6f1b8435e3d96890a361d2982d7b89b23585ca6
gaylesha <petr.vasilyev.1999@gmail.com>
2025-05-07 23:17:03 +0500

    init

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/Petro-vich/stepik.PRO.Go/src/2_1/task5"
+	"github.com/Petro-vich/stepik.PRO.Go/src/2_1/task9"
+)
+
+func main() {
+	task5.MainTask5()
+	task9.MainTask9()
+}
