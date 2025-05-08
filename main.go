b49c39c42f51696876a28d8ef674fe042ccba0be
gaylesha <petr.vasilyev.1999@gmail.com>
2025-05-08 11:41:32 +0500

    task4

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/Petro-vich/stepik.PRO.Go/src/2_2/task4"
 	"github.com/Petro-vich/stepik.PRO.Go/src/2_1/task5"
 	"github.com/Petro-vich/stepik.PRO.Go/src/2_1/task9"
+	"github.com/Petro-vich/stepik.PRO.Go/src/2_2/task4"
 )
 
 func main() {
